351344abffef1f97043d7710498b9e563a1f5188
Liang Ji Zhu <www.1159068228@gmail.com>
2024-04-09 02:00:59 +0200

    asd

--- business_way_of_life.sql
+++ business_way_of_life.sql
@@ -84,54 +84,54 @@ WHERE la.barcode = 'QIQ79610I616590'
 ORDER BY orderdate;
 
 
--- Prepare client sales data for the last month
-WITH ClientSales AS (
-    SELECT
-        cl.barcode, 
-        cl.orderdate AS sale_date, 
-        TO_NUMBER(cl.quantity) AS quantity,
-        cl.price,
-        sl.cost
-    FROM Client_Lines cl
-    JOIN References r ON cl.barcode = r.barcode
-    LEFT JOIN Supply_Lines sl ON r.barCode = sl.barCode
-    WHERE cl.orderdate >= TRUNC(ADD_MONTHS(SYSDATE, -5), 'MM')
-      AND cl.orderdate < TRUNC(SYSDATE, 'MM')
-),
--- Prepare data from anonymous sales for the last month
-AnonymSales AS (
-    SELECT
-        la.barcode, 
-        la.orderdate AS sale_date, 
-        la.quantity,
-        la.price,
-        sl.cost
-    FROM Lines_Anonym la
-    JOIN References r ON la.barcode = r.barcode
-    LEFT JOIN Supply_Lines sl ON r.barCode = sl.barCode
-    WHERE la.orderdate >= TRUNC(ADD_MONTHS(SYSDATE, -5), 'MM')
-      AND la.orderdate < TRUNC(SYSDATE, 'MM')
-),
--- Combine client and anonymous sales to calculate last month's sales statistics
-MonthlySales AS (
-    SELECT
-        cs.barcode, 
-        SUM(cs.quantity) AS units_sold,
-        SUM(cs.price * cs.quantity) AS total_income,
-        SUM(cs.price * cs.quantity) - SUM(cs.cost * cs.quantity) AS total_benefit,
-        ROW_NUMBER() OVER (ORDER BY SUM(cs.quantity) DESC) AS rank
-    FROM (
-        SELECT * FROM ClientSales
-        UNION ALL
-        SELECT * FROM AnonymSales
-    ) cs
-    GROUP BY cs.barcode
-)
--- Select the best-selling product for the last month
-SELECT
-    barcode AS best_selling_reference, 
-    units_sold,
-    total_income, 
-    total_benefit
-FROM MonthlySales
-WHERE rank = 1;
+-- -- Prepare client sales data for the last month
+-- WITH ClientSales AS (
+--     SELECT
+--         cl.barcode, 
+--         cl.orderdate AS sale_date, 
+--         TO_NUMBER(cl.quantity) AS quantity,
+--         cl.price,
+--         sl.cost
+--     FROM Client_Lines cl
+--     JOIN References r ON cl.barcode = r.barcode
+--     LEFT JOIN Supply_Lines sl ON r.barCode = sl.barCode
+--     WHERE cl.orderdate >= TRUNC(ADD_MONTHS(SYSDATE, -5), 'MM')
+--       AND cl.orderdate < TRUNC(SYSDATE, 'MM')
+-- ),
+-- -- Prepare data from anonymous sales for the last month
+-- AnonymSales AS (
+--     SELECT
+--         la.barcode, 
+--         la.orderdate AS sale_date, 
+--         la.quantity,
+--         la.price,
+--         sl.cost
+--     FROM Lines_Anonym la
+--     JOIN References r ON la.barcode = r.barcode
+--     LEFT JOIN Supply_Lines sl ON r.barCode = sl.barCode
+--     WHERE la.orderdate >= TRUNC(ADD_MONTHS(SYSDATE, -5), 'MM')
+--       AND la.orderdate < TRUNC(SYSDATE, 'MM')
+-- ),
+-- -- Combine client and anonymous sales to calculate last month's sales statistics
+-- MonthlySales AS (
+--     SELECT
+--         cs.barcode, 
+--         SUM(cs.quantity) AS units_sold,
+--         SUM(cs.price * cs.quantity) AS total_income,
+--         SUM(cs.price * cs.quantity) - SUM(cs.cost * cs.quantity) AS total_benefit,
+--         ROW_NUMBER() OVER (ORDER BY SUM(cs.quantity) DESC) AS rank
+--     FROM (
+--         SELECT * FROM ClientSales
+--         UNION ALL
+--         SELECT * FROM AnonymSales
+--     ) cs
+--     GROUP BY cs.barcode
+-- )
+-- -- Select the best-selling product for the last month
+-- SELECT
+--     barcode AS best_selling_reference, 
+--     units_sold,
+--     total_income, 
+--     total_benefit
+-- FROM MonthlySales
+-- WHERE rank = 1;
